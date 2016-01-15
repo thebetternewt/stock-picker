319b8a0c99b0c216940efa11763c94def845d304
Chris Eady <chris.eady1@gmail.com>
2016-01-14 18:23:53 -0600

    app now detects and corrects edge cases of hi and low buy/sell days

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,8 +1,27 @@
 stock_prices = [10,8,6,4,2,9,7,5,3,1]
 
-stock_prices_sorted = stock_prices.sort
-buy_price = stock_prices_sorted.min
-sell_price = stock_prices_sorted.max
+puts stock_prices.length
+
+buy_price = stock_prices.min
+buy_day = stock_prices.index(buy_price)
+
+sell_price = stock_prices.max
+sell_day = stock_prices.index(sell_price)
 
 puts "buy_price: #{buy_price}, sell_price: #{sell_price}"
-puts "buy day: #{stock_prices.index(buy_price)}, sell day: #{stock_prices.index(sell_price)}"
+puts "buy day: #{buy_day}, sell day: #{sell_day}"
+
+
+while sell_day <= buy_day
+  if sell_day == 0
+    sell_day = stock_prices.length-1
+    sell_price = stock_prices[sell_day]
+  elsif buy_day == stock_prices.length-1
+    puts "yes"
+    buy_day = 0
+    buy_price = stock_prices[buy_day]
+  end
+end
+
+puts "new buy_price: #{buy_price}, sell_price: #{sell_price}"
+puts "new buy day: #{buy_day}, sell day: #{sell_day}"
