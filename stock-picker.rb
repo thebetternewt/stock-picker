b3ee888b0d6f8714ac4492ecd67fc4fdaf26c83f
Chris Eady <chris.eady1@gmail.com>
2016-01-15 16:16:53 -0600

    correctly corrects buying on last day and selling on first

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,7 +1,9 @@
 stock_prices = [10,8,6,4,2,9,7,5,3,1]
 
-puts stock_prices.length
+puts "Stock prices: #{stock_prices}"
+puts "Number of days: #{stock_prices.length}"
 
+# Buy = Min, Sell = Max
 buy_price = stock_prices.min
 buy_day = stock_prices.index(buy_price)
 
@@ -11,17 +13,21 @@ sell_day = stock_prices.index(sell_price)
 puts "buy_price: #{buy_price}, sell_price: #{sell_price}"
 puts "buy day: #{buy_day}, sell day: #{sell_day}"
 
+stock_prices_sorted = stock_prices.sort
 
-while sell_day <= buy_day
-  if sell_day == 0
-    sell_day = stock_prices.length-1
-    sell_price = stock_prices[sell_day]
-  elsif buy_day == stock_prices.length-1
-    puts "yes"
-    buy_day = 0
-    buy_price = stock_prices[buy_day]
+index_modifier = 1
+#while sell_day <= buy_day
+# Don't buy on last day
+  if buy_day == stock_prices.length-1
+    buy_price = stock_prices_sorted[stock_prices_sorted.index(buy_price) + index_modifier]
+    buy_day = stock_prices.index(buy_price)
   end
-end
+  # Don't sell on first day
+  if sell_day == 0
+    sell_price = stock_prices_sorted[stock_prices_sorted.index(sell_price) - index_modifier]
+    sell_day = stock_prices.index(sell_price)
+  end
+#end
 
 puts "new buy_price: #{buy_price}, sell_price: #{sell_price}"
 puts "new buy day: #{buy_day}, sell day: #{sell_day}"
